555421015c0e0597407d64cf5695a6530629a071
Shanel <shanelpickard@gmail.com>
2016-06-13 17:38:39 -0400

    TestSpikeCount produces correct output

--- TestSpikeCount2.py
+++ TestSpikeCount2.py
@@ -1,25 +1,31 @@
 import numpy as np
+import itertools
 
-#Mock data is collected over a time horizon of 0-1sec
-#Goal is to find how many spike occur in each sub time interval
+##Mock data is collected over a time horizon of 0-1sec
+##Goal is to find how many spike occur in each sub time interval
 
-#Run time - specified by user for the simulation
-duration = 5
-#Spike time array
+##Run time - specified by user for the simulation
+duration = 5    #units: seconds
+##Practice spike time array; this array output consists of the number of spikes occuring in the network for each time
+## subinterval
 time = [.1, 1.3, 1.4, 2.2, 2.7, 3.5, 4.3, 4.4]
 # time = [0, 0.55555556,1.11111111,1.66666667,2.22222222,2.77777778, 3.33333333,3.88888889,4.44444444,5]
-time = np.array(time)
+time = np.array(time)       #make time list an array
 print ('time = ', time)
 #Interval array - intervals in which to break up the time array - sub time interval array
 dt = .2                                     #User defined time interval in to which count spikes over
 n = duration/dt                             #How many subintervals from time horizun results from user defined interval
 print (n)
 splitInterval = np.linspace(0,duration, n+1)    #Generate a interval array to find the number of spikes occuring in each
-print ('splitInterval: ', splitInterval)                                                #interval
-length_splitInt = len(splitInterval)
-length_time = len(time)
-length = length_splitInt + (length_time-2)
-print ('length :', length_splitInt)
+print ('splitInterval: ', splitInterval)
+
+##Trying different lengths over which to iterate in for loop
+# length_splitInt = len(splitInterval)
+# print ('length splitInterval: ', length_splitInt)
+# length_time = len(time)
+# print ('length time: ',length_time)
+# length = length_splitInt + (length_time)
+# print ('length :', length)
 
 i=0     #inex for for subinterval (length of splitInterval)
 j=0     #index for splitInterval array.
@@ -28,19 +34,27 @@ k = 0   #index for new matrix that will store the grouped values from the split 
 counter = 0
 SpikeCount = []
 
-for i in range(length):
+for i in range(len(splitInterval):
     if (time[k] >= splitInterval[j]) and (time[k] <= splitInterval[j + 1]):
         print ('time element: ', time[k])
         print ('splitInt: ', splitInterval[j], splitInterval[j+1])
         counter += 1
         print('if counter: ', counter)
         i += 1
-        k+=1
-        print ('k: ', k)
         print('i: ', i)
+        if k > len(time - 1):
+            print ('k:time element: ', k,time)
+            counter = 0
+            SpikeCount.append(counter)
+            print(SpikeCount)
+            print('k: ', k)
+            print('stop k count')
+        else:
+            k+=1
+            print ('if if k: ', k)
     else:
         SpikeCount.append(counter)
-        print (SpikeCount)
+        print(SpikeCount)
         print('time element: ', time[k])
         print('splitInt: ', splitInterval[j], splitInterval[j + 1])
         counter = 0
@@ -50,6 +64,8 @@ for i in range(length):
         i+=1
         print('i: ', i)
         print ('k: ', k)
+
+
 
 SpikeCount.append(counter)
 print('Spike count: ', SpikeCount)
--- TestSpikeCount3.py
+++ TestSpikeCount3.py
@@ -1,25 +1,32 @@
 import numpy as np
+import itertools
 
-#Mock data is collected over a time horizon of 0-1sec
-#Goal is to find how many spike occur in each sub time interval
+##Mock data is collected over a time horizon of 0-1sec
+##Goal is to find how many spike occur in each sub time interval
 
-#Run time - specified by user for the simulation
-duration = 5
-#Spike time array
-time = [.1, 1.3, 1.4, 2.2, 2.7, 3.5, 4.3, 4.4]
+##Run time - specified by user for the simulation
+duration = 5    #units: seconds
+##Practice spike time array; this array output consists of the number of spikes occuring in the network for each time
+## subinterval
+time = [.1, 1.3, 1.4, 2.2, 2.7, 3.5, 4.3, 4.4, 4.4]
 # time = [0, 0.55555556,1.11111111,1.66666667,2.22222222,2.77777778, 3.33333333,3.88888889,4.44444444,5]
-time = np.array(time)
+time = np.array(time)       #make time list an array
 print ('time = ', time)
 #Interval array - intervals in which to break up the time array - sub time interval array
 dt = .2                                     #User defined time interval in to which count spikes over
 n = duration/dt                             #How many subintervals from time horizun results from user defined interval
 print (n)
 splitInterval = np.linspace(0,duration, n+1)    #Generate a interval array to find the number of spikes occuring in each
-print ('splitInterval: ', splitInterval)                                                #interval
+print ('splitInterval: ', splitInterval)
+
+
+##Trying different lengths over which to iterate in for loop
 length_splitInt = len(splitInterval)
+print ('length splitInterval: ', length_splitInt)
 length_time = len(time)
-length = length_splitInt + (length_time-2)
-print ('length :', length_splitInt)
+print ('length time: ',length_time)
+length = length_splitInt + ((length_time)-2)
+print ('length :', length)
 
 i=0     #inex for for subinterval (length of splitInterval)
 j=0     #index for splitInterval array.
@@ -29,27 +36,38 @@ counter = 0
 SpikeCount = []
 
 for i in range(length):
-    if (time[k] >= splitInterval[j]) and (time[k] <= splitInterval[j + 1]):
+    if (time[k] > splitInterval[j]) and (time[k] <= splitInterval[j + 1]):
         print ('time element: ', time[k])
         print ('splitInt: ', splitInterval[j], splitInterval[j+1])
         counter += 1
         print('if counter: ', counter)
         i += 1
-        k+=1
-        print ('k: ', k)
         print('i: ', i)
+        print('if k: ', k)
+        if k < (len(time)-1):
+            k += 1
+            print('iff k: ', k)
+            print('iff counter: ', counter)
+        else:
+            print('iff counter: ', counter)
+            # SpikeCount.append(counter)
+            print(SpikeCount)
+            j += 1
+            print('iff j: ', j)
     else:
         SpikeCount.append(counter)
-        print (SpikeCount)
+        print(SpikeCount)
         print('time element: ', time[k])
         print('splitInt: ', splitInterval[j], splitInterval[j + 1])
         counter = 0
         print('else counter: ', counter)
         j += 1
-        print('j: ', j)
+        print('else j: ', j)
         i+=1
-        print('i: ', i)
-        print ('k: ', k)
+        print('else i: ', i)
+        print ('else k: ', k)
+
+
 
 SpikeCount.append(counter)
 print('Spike count: ', SpikeCount)
