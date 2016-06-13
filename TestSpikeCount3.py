f3d49e5fd2733260c03813ff95d72fd9a92f8d90
Shanel <shanelpickard@gmail.com>
2016-06-13 18:33:52 -0400

    TestSpikeCount3 still modifying for loop

--- TestSpikeCount3.py
+++ TestSpikeCount3.py
@@ -1,3 +1,5 @@
+#Updated 6/13/16
+
 import numpy as np
 import itertools
 
@@ -8,8 +10,8 @@ import itertools
 duration = 5    #units: seconds
 ##Practice spike time array; this array output consists of the number of spikes occuring in the network for each time
 ## subinterval
-time = [.1, 1.3, 1.4, 2.2, 2.7, 3.5, 4.3, 4.4, 4.4]
-# time = [0, 0.55555556,1.11111111,1.66666667,2.22222222,2.77777778, 3.33333333,3.88888889,4.44444444,5]
+# time = [.1, 1.3, 1.4, 2.2, 2.7, 3.5, 4.3, 4.4]
+time = [0, 0.55555556,1.11111111,1.66666667,2.22222222,2.77777778, 3.33333333,3.88888889,4.44444444,5]
 time = np.array(time)       #make time list an array
 print ('time = ', time)
 #Interval array - intervals in which to break up the time array - sub time interval array
@@ -36,34 +38,42 @@ counter = 0
 SpikeCount = []
 
 for i in range(length):
-    if (time[k] > splitInterval[j]) and (time[k] <= splitInterval[j + 1]):
-        print ('time element: ', time[k])
-        print ('splitInt: ', splitInterval[j], splitInterval[j+1])
+    if (time[k] >= splitInterval[j]) and (time[k] <= splitInterval[j + 1]):
         counter += 1
-        print('if counter: ', counter)
         i += 1
+
+        #Spot check
+        print('if counter: ', counter)
+        print('time element: ', time[k])
+        print('splitInt: ', splitInterval[j], splitInterval[j + 1])
         print('i: ', i)
         print('if k: ', k)
+
         if k < (len(time)-1):
             k += 1
+
+            # Spot check
             print('iff k: ', k)
             print('iff counter: ', counter)
         else:
-            print('iff counter: ', counter)
-            # SpikeCount.append(counter)
-            print(SpikeCount)
             j += 1
+            # Spot check
+
+            print('iff counter: ', counter)
+            print(SpikeCount)
             print('iff j: ', j)
     else:
         SpikeCount.append(counter)
+        counter = 0
+        j += 1
+        i+=1
+
+        #Spot Check
+        print('else counter: ', counter)
         print(SpikeCount)
         print('time element: ', time[k])
         print('splitInt: ', splitInterval[j], splitInterval[j + 1])
-        counter = 0
-        print('else counter: ', counter)
-        j += 1
         print('else j: ', j)
-        i+=1
         print('else i: ', i)
         print ('else k: ', k)
 
