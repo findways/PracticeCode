01ce41d06cdce9db1bba9ad5fab4deda6497ac55
Shanel <shanelpickard@gmail.com>
2016-06-30 17:44:33 -0400

    fixing for loop

--- TestSpikeCount4.py
+++ TestSpikeCount4.py
@@ -12,21 +12,22 @@ duration = 5    #units: seconds
 
 ##Practice spike time array; this array output consists of the number of spikes occuring in the network for each time
 ## subinterval
-time = ([0, 1.3, 1.4, 2.2, 2.7, 3.5, 4.3],[.1, 1.3, 1.4, 2.2, 2.7, 3.5, 4.3, 4.4], [0, 1.3, 1.4, 2.2, 2.7, 3.5, 4.3, 4.4],[0, 1.3, 1.4, 2.2, 2.7, 3.5, 4.3])
+# time = ([0, 1.3, 1.4, 2.2, 2.7, 3.5],[.1, 1.3, 1.4, 2.2, 2.7, 3.5, 4.3, 4.4], [0, 1.3, 1.4, 2.2, 2.7, 3.5, 4.3, 4.4],[0, 1.3, 1.4, 2.2, 2.7, 3.5, 4.3])
+time = ([2.9131, 6.3563, 7.1718, 9.2685],[2.7223, 3.1245, 3.6034],[0.9104, 7.703 , 9.2128])
 # time = [1.80000000e-03, 2.00000000e-03, 2.10000000e-03, 9.99850000e+00, 9.99910000e+00, 9.99980000e+00]
 # time = [0, 0.55555556,1.11111111,1.66666667,2.22222222,2.77777778, 3.33333333,3.88888889,4.44444444,5]
 time = np.array(time)       #make time list an array
 # print ('time = ', time)
 
 #Interval array - intervals in which to break up the time array - sub time interval array
-dt = 2                                     #User defined time interval in to which count spikes over
+dt = 1                                     #User defined time interval in to which count spikes over
 n = duration/dt                             #How many subintervals from time horizun results from user defined interval
 # print (n)
 splitInterval = np.linspace(0,duration, n+1)    #Generate a interval array to find the number of spikes occuring in each
 # print ('splitInterval: ', splitInterval)
 # splitInterval = ([0, 2.5, 5], [0, 2.5, 5], [0, 2.5, 5])
 # splitInterval = np.array(splitInterval)
-# print ('splitInterval: ', splitInterval)
+print ('splitInterval: ', splitInterval)
 
 ##Trying different lengths over which to iterate in for loop
 # length_splitInt = len(splitInterval)
@@ -43,10 +44,7 @@ j=0     #index for splitInterval array.
 k = 0   #index for new matrix that will store the grouped values from the split time array
 n = 0   #row tracker
 counter = 0
-SpikeCount = [[] for col in range(len(time))]
-
-
-
+SpikeCount = [[] for m in range(len(time))]
 
 for row in range(len(time)+1):
     print ('row range')
@@ -100,8 +98,7 @@ for row in range(len(time)+1):
                 print('row: ', row)
                 print('col: ', col)
                 print('k: ', k)
-                print ('len(time[row]): ', len(time[row]))
-
+                print('len(time[row]): ', len(time[row]))
                 #if/else statements to determine which interator gets incremented
                 if k < (len(time[row])):
                     k += 1
@@ -110,40 +107,60 @@ for row in range(len(time)+1):
                     print('k: ', k)
                     print('counter: ', counter)
                     print('col: ', col)
-                else:
-                    j += 1
-                    counter+=1
-                    SpikeCount[row].append(counter)
-                    # Spot check
-                    print ('elif else')
-                    print ('row: ', row)
-                    print('col: ', col)
-                    print('counter: ', counter)
-                    print('SpikeCount: ', SpikeCount)
-                    print('j: ', j)
-                    print('len(time[row]): ', len(time[row]))
-
             else:
                 print('else2')
-                print('counter: ', counter)
                 SpikeCount[row].append(counter)
-
+                j += 1
+                print('counter: ', counter)
+                print('SpikeCount: ', SpikeCount)
+                print('j: ', j)
                 if (k==len(time[row])):
+                    print ('else2if')
+                    if (j<len(splitInterval)):
+                        print('else2ifif')
+                        counter = 0
+                        SpikeCount[row].append(counter)
+                        print('counter: ', counter)
+                        print('SpikeCount: ', SpikeCount)
+                        print('j: ', j)
+                        print('col: ', col)
+                        print('k: ', k)
+                        print('row: ', row)
+                    else:
+                        print ('else2ifelse')
+                        j = 0
+                        k = 0
+                        counter = 0
+                        print('counter: ', counter)
+                        print('SpikeCount: ', SpikeCount)
+                        print('j: ', j)
+                        print('col: ', col)
+                        print('k: ', k)
+                        print('row: ', row)
+                elif (j == (len(splitInterval)-1)):
+                    print ('else2elif')
                     j = 0
                     k = 0
                     counter = 0
+                    print('counter: ', counter)
+                    print('SpikeCount: ', SpikeCount)
+                    print('j: ', j)
+                    print('col: ', col)
+                    print('k: ', k)
+                    print('row: ', row)
+                    print('break')
+                    break
                 else:
-                    j += 1
+                    print('else2else')
+                    # j += 1
                     counter = 0
-
-                # Spot Check
-                print('counter: ', counter)
-                print('SpikeCount: ', SpikeCount)
-                # print('splitInt: ', splitInterval[j], splitInterval[j + 1])
-                print('j: ', j)
-                print('col: ', col)
-                print('k: ', k)
-                print('row: ', row)
+                    print('time element: ', time[row][k])
+                    print('splitInt: ', splitInterval[j], splitInterval[j + 1])
+                    print('counter: ', counter)
+                    print('j: ', j)
+                    print('col: ', col)
+                    print('k: ', k)
+                    print('row: ', row)
 
     else:
         print ('else1')
